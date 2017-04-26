11c57bc613340a2498065268f2fbea9d0aecf436
Marco Worms <marcogworms@gmail.com>
2017-04-26 12:40:00 -0300

    prettify js

--- src/CanvasComponent.jsx
+++ src/CanvasComponent.jsx
@@ -1,71 +1,91 @@
-import React from 'react';
+import React from 'react'
+
+// this could be "values" from ramda
+const values = (object) => {
+  return Object.keys(object)
+    .map(key => object[key])
+}
 
 function polygon(props) {
-    const {ctx, points} = props
+  const {ctx, points} = props
 
-    const objectToArray = (object) => {
-        return Object.keys(object).map(function (key) { return object[key] })
-    }
-
-    props.points.forEach((point, index) => {
-        point = objectToArray(point)
-        index <= 0 ? 
-            ctx.moveTo(...point):
-            ctx.lineTo(...point)
-    })
+  props.points.forEach((point, index) => {
+    cords = values(point)
+    index <= 0
+      ? ctx.moveTo(...cords)
+      : ctx.lineTo(...cords)
+  })
 }
 
 function drawPolygon(props) {
-    polygon(props)
-    const {ctx} = props
-    ctx.fill()
+  polygon(props)
+  const {ctx} = props
+  ctx.fill()
 }
 
 class CanvasComponent extends React.Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props)
 
-        this.state = {
-            points: [],
-            mouseX:0, 
-            mouseY:0
-        }
+    this.state = {
+      points: [],
+      mouseX: 0, 
+      mouseY: 0
     }
+  }
 
-    componentDidMount() {
-        this.updateCanvas();
+  componentDidMount() {
+    this.updateCanvas()
+  }
+
+  updateCanvas() {
+    const ctx = this.refs.canvas.getContext('2d')
+  }
+
+  handleOnClick() {
+    const ctx = this.refs.canvas.getContext('2d')
+
+    const point = {
+      x: this.state.mouseX,
+      y: this.state.mouseY,
     }
+    
+    const points = [
+      ...this.state.points,
+      point,
+    ]
 
-    updateCanvas() {
-        const ctx = this.refs.canvas.getContext('2d')
-    }
+    this.setState(
+      { points },
+      () => { drawPolygon({
+        points: this.state.points,
+        ctx,
+      })
+    })
+  }
 
-    handleOnClick() {
-        const ctx = this.refs.canvas.getContext('2d')
+  handleMouseMove(e) {
+    this.setState({
+      mouseX: e.pageX,
+      mouseY:e.pageY,
+    })
+  }
 
-        const points = this.state.points.slice()
+  handleOnHold() {
+  }
 
-        const point = {x:this.state.mouseX, y:this.state.mouseY}
-
-        points.push(point)
-
-        this.setState({points:points}, () => {
-            drawPolygon({ctx, points:this.state.points})
-        })
-    }
-
-    handleMouseMove(e) {
-        this.setState({mouseX:e.pageX, mouseY:e.pageY})
-    }
-
-    handleOnHold() {
-    }
-
-    render() {
-        return (
-            <canvas ref="canvas" onMouseMove={this.handleMouseMove.bind(this)} onClick={this.handleOnClick.bind(this)} style={{border : this.props.border}} width={300} height={300}/>
-        );
-    }
+  render() {
+    return (
+      <canvas
+        ref="canvas"
+        onMouseMove={this.handleMouseMove.bind(this)}
+        onClick={this.handleOnClick.bind(this)}
+        style={{border: this.props.border}}
+        width={300}
+        height={300}
+      />
+    )
+  }
 }
 
-export default CanvasComponent;
+export default CanvasComponent
